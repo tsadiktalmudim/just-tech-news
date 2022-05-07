fdb3f65e3511eee4663eb3ae81712deebab8da08
Xan <tsadiktalmudim@gmail.com>
2022-05-07 16:41:36 -0500

    login/logout and session functionality added

---
+++ public/javascript/logout.js
@@ -0,0 +1,16 @@
+const { post } = require("../../controllers");
+
+async function logout() {
+  const response = await fetch("api/users/logout", {
+    method: post,
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector("#logout").addEventListener("click", logout);
